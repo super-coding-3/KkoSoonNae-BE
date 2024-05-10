f7ea33d12c0e39ed62453f81a7a46e8749ba4f0a
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-05-10 16:24:52 +0900

    [setting] 초기 커밋

---
+++ src/test/java/com/kkosoonnae/KkosoonnaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkosoonnae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkosoonnaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
