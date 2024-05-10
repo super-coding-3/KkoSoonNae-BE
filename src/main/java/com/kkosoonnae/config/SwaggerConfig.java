233f89953f4f74c83f4f0ed41b543b2fd7657fa7
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-05-10 23:51:31 +0900

    [feature] jwt 토큰 발급 및 로그인 , 회원가입 구현 완료

---
+++ src/main/java/com/kkosoonnae/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.kkosoonnae.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+/**
+ * packageName    : com.kurly.api.config
+ * fileName       : SwaggerConfig
+ * author         : hagjoon
+ * date           : 2024-04-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-24        hagjoon       최초 생성
+ */
+@OpenAPIDefinition(
+        info = @Info(title = "KkoSoonNae API 명세서",
+        description = "꼬순내 API 명세서",
+        version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("Bearer ").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
+}
