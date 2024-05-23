8968e1ab2427a5c5332c40813e90e900e26d8977
kimjaeik95 <wodlrdlsla@naver.com>
2024-05-23 12:08:09 +0900

    [hotfix:컨트롤러 임시예외처리]

--- src/main/java/com/kkosoonnae/store/controller/StoreController.java
+++ src/main/java/com/kkosoonnae/store/controller/StoreController.java
@@ -1,8 +1,6 @@
 package com.kkosoonnae.store.controller;
 
-import com.kkosoonnae.search.dto.StoreListViewResponseDto;
 import com.kkosoonnae.store.dto.*;
-import com.kkosoonnae.store.exception.CustomException;
 import com.kkosoonnae.store.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.webjars.NotFoundException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * packageName    : com.kkosoonnae.store.controller
@@ -36,29 +35,33 @@ public class StoreController {
 
     @GetMapping("/{storeNo}")
     @Operation(summary = "상세매장조회")
-    public ResponseEntity<StoreDetailWithImageResponseDto> StoreDetailWithImage(@PathVariable Integer storeNo) {
+    public ResponseEntity<?> StoreDetailWithImage(@PathVariable Integer storeNo) {
         try {
             log.info("GET/Store 조회요청이 들어왔습니다.StoreNo:" + storeNo);
             StoreDetailWithImageResponseDto storeDetailWithImageResponseDto = storeService.findStoreDetailWithImage(storeNo);
             log.info("GET/Store 조회응답:" + storeDetailWithImageResponseDto);
             return ResponseEntity.ok().body(storeDetailWithImageResponseDto);
-        } catch (NotFoundException e) {
+        } catch (Exception e) {
+            Map<String, String> errorBody = new HashMap<>();
+            errorBody.put("error", "매장이 없습니다.");
             log.info("Client 요청에 문제가 있어 다음 오류를 출력합니다.:" + e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
         }
     }
 
     @GetMapping("/{storeNo}/petHair")
     @Operation(summary = "펫스타일조회")
-    public ResponseEntity<List<StyleDto>> AllStyles(@PathVariable Integer storeNo) {
+    public ResponseEntity<?> AllStyles(@PathVariable Integer storeNo) {
         try {
             log.info("GET/storeNo로 펫스타일 조회요청이 들어왔습니다.:" + storeNo);
             List<StyleDto> styleDto = storeService.findStyles(storeNo);
             log.info("GET/petHair 응답:" + styleDto);
             return ResponseEntity.ok(styleDto);
-        } catch (NotFoundException e) {
+        } catch (Exception e) {
+            Map<String, String> errorBody = new HashMap<>();
+            errorBody.put("error", "펫스타일이 없습니다.");
             log.info("Client 요청에 문제가 있어 다음 오류를 출력합니다.:" + e.getMessage());
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
 
         }
     }
@@ -71,9 +74,11 @@ public class StoreController {
             LikeStoreDto likeStoreDto = storeService.saveLikeStore(customerNo, storeNo);
             log.info("POST/LikeStore 응답조회:" + likeStoreDto);
             return ResponseEntity.ok(likeStoreDto);
-        } catch (NotFoundException e) {
+        } catch (Exception e) {
+            Map<String, String> errorBody = new HashMap<>();
+            errorBody.put("error", "관심매장이 중복입니다.");
             log.info("Client 요청에 문제가 있어 다음 오류를 출력합니다.:" + e.getMessage());
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(errorBody);
         }
     }
 
@@ -84,13 +89,17 @@ public class StoreController {
         try {
             log.info("POST/customerNo,storeNo 관심매장삭제 요청이 들어왔습니다.");
             storeService.deleteLikeStore(customerNo, storeNo);
-            log.info("POST/관심매장삭제 응답" );
+            log.info("POST/관심매장삭제 응답");
             return ResponseEntity.noContent().build();
-        } catch (NotFoundException e) {
+        } catch (Exception e) {
+            Map<String, String> errorBody = new HashMap<>();
+            errorBody.put("error", "관심매장이 없습니다.");
             log.info("Client 요청에 문제가 있어 다음 오류를 출력합니다.:" + e.getMessage());
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e);
         }
     }
+
+
     @PostMapping("/review")
     @Operation(summary = "리뷰 작성")
     public ResponseEntity<ReviewResponseDto> createReview(@RequestBody ReviewDto reviewDto) {
