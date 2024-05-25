aefe569d893dcc883b40a255b4e51abe8e5a7a1c
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-05-26 08:21:03 +0900

    [fix] test

--- src/main/java/com/kkosoonnae/config/s3/S3Config.java
+++ src/main/java/com/kkosoonnae/config/s3/S3Config.java
@@ -1,45 +1,45 @@
-//package com.kkosoonnae.config.s3;
-//
-//import com.amazonaws.auth.AWSCredentials;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * packageName    : com.kkosoonnae.config.s3
-// * fileName       : S3Config
-// * author         : hagjoon
-// * date           : 2024-05-26
-// * description    :
-// * ===========================================================
-// * DATE              AUTHOR             NOTE
-// * -----------------------------------------------------------
-// * 2024-05-26        hagjoon       최초 생성
-// */
-//@Configuration
-//public class S3Config {
-//
-////    @Value("${cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//
-////    @Value("${cloud.aws.credentials.secret-key}")
-//    private String secretKey;
-//
-//    @Value("${cloud.aws.region.static}")
-//    private String region;
-//
-//    @Bean
-//    public AmazonS3 amazonS3Client() {
-//        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-//
-//        return AmazonS3ClientBuilder
-//                .standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .withRegion(region)
-//                .build();
-//    }
-//}
+package com.kkosoonnae.config.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * packageName    : com.kkosoonnae.config.s3
+ * fileName       : S3Config
+ * author         : hagjoon
+ * date           : 2024-05-26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-05-26        hagjoon       최초 생성
+ */
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
