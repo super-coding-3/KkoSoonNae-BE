d7ce0ddb78f329e2b878897a90cf4784a0c123ff
skfkfh <2d2y0103@gmail.com>
2024-05-26 00:13:01 +0900

    리뷰작성 수정

--- src/main/java/com/kkosoonnae/jpa/repository/ReviewRepository.java
+++ src/main/java/com/kkosoonnae/jpa/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.kkosoonnae.jpa.repository;
 
+import com.kkosoonnae.jpa.entity.CustomerBas;
 import com.kkosoonnae.jpa.entity.Review;
 import com.kkosoonnae.jpa.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +15,12 @@ public interface ReviewRepository extends JpaRepository<Review,Integer> {
     List<Review> findByStoreStoreNo(Integer storeId);
 
     Optional<Review> findTopByStoreStoreNoOrderByReviewDtDesc(Integer storeNo);
+
+    @Query("SELECT * " +
+            "FROM REVIEW r " +
+            "LEFT JOIN CUSTOMER_BAS cb" +
+            "ON r.CSTMR_NO = cb.CSTMR_NO" +
+            "LEFT JOIN STORE s " +
+            "ON r.STORE_NO = s.STORE_NO")
+    CustomerBas findCustomerByCustomerNumber(@Param("cstmrNo") Integer cstmrNo);
 }
