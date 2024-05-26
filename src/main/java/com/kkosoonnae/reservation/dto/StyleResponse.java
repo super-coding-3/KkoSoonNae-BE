285916670eda9c5fb086ae776d91f9dd96eeb4d7
keehan95 <colinbae95@gmail.com>
2024-05-27 01:24:55 +0900

    [feature] 스타일 불러올 때 가격 추가.

--- src/main/java/com/kkosoonnae/reservation/dto/StyleResponse.java
+++ src/main/java/com/kkosoonnae/reservation/dto/StyleResponse.java
@@ -3,7 +3,10 @@ package com.kkosoonnae.reservation.dto;
 import com.kkosoonnae.jpa.entity.Style;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @NoArgsConstructor
@@ -15,9 +18,17 @@ public class StyleResponse {
     @Schema(description = "스타일 이름")
     private String cutStyle;
 
+    @Schema(description = "스타일 가격")
+    private String price;
+
     public static StyleResponse styleToStyleResponse(Style style) {
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.KOREA);
+        String formattedPrice = numberFormat.format(style.getPrice());
+        String stringPrice = formattedPrice + "원";
+
         return StyleResponse.builder()
                 .cutStyle(style.getStyleName())
+                .price(stringPrice)
                 .build();
     }
 
