ddace808510fa5cea46448df75f56229ee20ea23
keehan95 <colinbae95@gmail.com>
2024-05-28 11:49:29 +0900

    [feature] 이미지 추가.

--- src/main/java/com/kkosoonnae/reservation/dto/PetResponse.java
+++ src/main/java/com/kkosoonnae/reservation/dto/PetResponse.java
@@ -18,6 +18,9 @@ public class PetResponse {
     @Schema(description = "펫 이름")
     private String petName;
 
+    @Schema(description = "펫 이미지")
+    private String petImg;
+
     @Schema(description = "견종/묘종")
     private String breed;
 
@@ -28,6 +31,7 @@ public class PetResponse {
         return PetResponse.builder()
                 .petNumber(pet.getPetNo())
                 .petName(pet.getName())
+                .petImg(pet.getImg())
                 .breed(pet.getType())
                 .weight(pet.getWeight())
                 .build();
