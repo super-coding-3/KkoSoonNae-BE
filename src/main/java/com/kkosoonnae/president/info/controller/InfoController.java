960d04884b8da165d506c459a6046cc8d91e4b1a
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-06-22 20:27:16 +0900

    [feature] 사장단 회원 가입 로직

--- src/main/java/com/kkosoonnae/president/info/controller/InfoController.java
+++ src/main/java/com/kkosoonnae/president/info/controller/InfoController.java
@@ -1,10 +1,18 @@
 package com.kkosoonnae.president.info.controller;
 
+import com.kkosoonnae.president.info.dto.SignUpDto;
+import com.kkosoonnae.president.info.service.InfoService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 /**
  * packageName    : com.kkosoonnae.president.info.controller
@@ -23,4 +31,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class InfoController {
+
+    private final InfoService infoService;
+
+    @PostMapping("/signup")
+    @Operation(summary = "사장 회원 가입")
+    public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto){
+        try {
+            boolean isSuccess = infoService.signUp(signUpDto);
+            if(isSuccess){
+                return ResponseEntity.ok(Collections.singletonMap("message","회원가입에 성공하였습니다."));
+            }else {
+                return ResponseEntity.badRequest().body(Collections.singletonMap("message","회원가입에 실패했습니다."));
+            }
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(Collections.singletonMap("message", e.getMessage()));
+        }
+    }
 }
---
+++ src/main/java/com/kkosoonnae/president/info/dto/SignUpDto.java
@@ -0,0 +1,40 @@
+package com.kkosoonnae.president.info.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : com.kkosoonnae.president.info.dto
+ * fileName       : SignUpDto
+ * author         : hagjoon
+ * date           : 2024-06-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-06-20        hagjoon       최초 생성
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDto {
+
+    @Schema(description = "로그인 ID")
+    private String loginId;
+
+    @Schema(description = "비밀번호")
+    private String password;
+
+    @Schema(description = "이메일")
+    private String email;
+
+    @Schema(description = "핸드폰 번호")
+    private String phone;
+
+    @Schema(description = "닉네임")
+    private String nickName;
+}
